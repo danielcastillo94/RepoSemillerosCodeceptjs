e9e1e553083c5148a063712b10827eed4981095f
Emilio <herreraemilio459@gmail.com>
2025-04-15 10:41:56 -0600

    Version2

---
+++ FRAMEWORKCODE/features/prueba.feature
@@ -0,0 +1,29 @@
+Feature: Prueba pagina inicial de telcel
+
+Scenario Outline: Prueba busqueda en telcel
+    Given Estoy en telcel
+    When Buscar "<producto>"
+    Then ver los resultados en la busqueda
+
+Examples:
+|producto|
+|iphone|
+
+@pago
+Scenario: Presionar boton de paga tu factura
+    Given Estoy en telcel
+    When Presiono el boton de paga tu factura
+    And Escribo el número de teléfono
+    And Escribo el número adicional
+    And Presiono el botón de continuar si está habilitado
+    Then verifico si el botón de continuar estaba habilitado
+
+
+@carrito
+Scenario: Agregar dos productos al carrito
+    Given Estoy en telcel
+    When Presiono el boton para entrar a la tienda en linea
+    And Presiono el boton de telefonos
+    And presiono el boton de Lo quiero para el primer resultado
+    And presiono el boton de agregar al carrito
+    Then presiono el boton de ir al carrito para ver si se agrego
---
+++ FRAMEWORKCODE/steps/pruebasteps.js
@@ -0,0 +1,84 @@
+const { I } = inject();
+
+Given("Estoy en telcel", () => {
+    I.amOnPage("/");
+    I.wait(2);
+});
+
+When(/^Buscar "([^"]*)"$/, (producto) => {
+    global.productoBuscado = producto;
+    I.fillField("#buscador-menu-input", producto);
+    I.pressKey("Enter");
+    I.wait(2);
+});
+
+Then("ver los resultados en la busqueda", async () => {
+    const xpath = `//span[contains(text(),"${global.productoBuscado}")]`;
+    I.waitForElement(xpath, 5);
+});
+
+When("Presiono el boton de paga tu factura", () => {
+    I.waitForElement('(//a[@class="center-block"])[2]', 5);
+    I.click('(//a[@class="center-block"])[2]');
+    I.wait(3); 
+});
+
+
+When("Escribo el número de teléfono", () => {
+    I.waitForEnabled('//input[@name="mdn"]', 10);
+    I.click('//input[@name="mdn"]')
+    I.fillField('//input[@name="mdn"]', '5522502546');
+});
+
+When("Escribo el número adicional", () => {
+    I.click('//input[@name="confirmMdn"]')
+    I.fillField('//input[@name="confirmMdn"]', '5522502546');
+});
+
+When("Presiono el botón de continuar si está habilitado", async () => {
+    const isDisabled = await I.grabAttributeFrom('//*[@id="root"]/div/div/form/div[2]/div/button', 'disabled');
+
+    if (!isDisabled) {
+        I.click('//*[@id="root"]/div/div/form/div[2]/div/button');
+        console.log("Botón habilitado. Se presionó correctamente.");
+    } else {
+        console.log("Botón deshabilitado. No se pudo presionar.");
+    }
+});
+
+Then("verifico si el botón de continuar estaba habilitado", async () => {
+    const isDisabled = await I.grabAttributeFrom('//*[@id="root"]/div/div/form/div[2]/div/button', 'disabled');
+
+    if (isDisabled) {
+        console.log("No se puede continuar. El número ingresado no es válido.");
+    } else {
+        console.log("Se puede continuar. El número es válido.");
+    }
+});
+
+When("Presiono el boton para entrar a la tienda en linea", async () => {
+    I.click("//img[contains(@title, 'Tienda en Línea')]");
+    I.wait(2)
+});
+
+When("Presiono el boton de telefonos", async () => {
+    I.click('(//img[@class="responsive desktop ng-star-inserted"])[1]')
+    I.wait(2)
+});
+
+When("presiono el boton de Lo quiero para el primer resultado", async () => {
+    I.click('(//button[@class="btn btn-primary btn-block btn-lo-quiero"])[1]')
+    I.wait(2)
+});
+
+When("presiono el boton de agregar al carrito", async () => {
+    I.click('//button[@class="btn btn-secondary addtominicart"]')
+    I.wait(2)
+});
+
+Then("presiono el boton de ir al carrito para ver si se agrego", async () => {
+    I.click('//a[@class="btn btn-primary cart"]')
+    I.wait(2)
+});
+
+
