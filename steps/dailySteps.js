3647abe9f66dc558c5f02188c97db6166c1b4e02
Administrator <administrator@MacBook-Pro-Lesly-Nohemi-Araiza.local>
2025-04-23 12:27:33 -0600

    dailymotion

--- steps/dailySteps.js
+++ steps/dailySteps.js
@@ -1,13 +1,11 @@
-const { I } = inject();
 const dailyPage = require('../pages/dailyPage.js');
-
 
 Given('navego a la pagina', () => {
   dailyPage.Home();
 });
 
 When('espero a que el contenido cargue', async () => {
-  await dailyPage.waitForContent();
+  await dailyPage.waitForCompleteLoad(); 
 });
 
 Then('verifico que se cargue el contenido', async () => {
