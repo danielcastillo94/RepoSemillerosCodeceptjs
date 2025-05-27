0985c905fdf0a7090bfa3b4c5f0ea77e3419aa72
Ahorra <jmacias@ahorra.io>
2025-05-27 11:03:40 -0600

    Se subira la actividad

--- codecept.conf.js
+++ codecept.conf.js
@@ -6,49 +6,49 @@ exports.config = {
     Puppeteer: {
       url: 'https://www.youtube.com',
       show: false,
-
       locale: "es-MX",
     },
     MiHelper: {
       require: './helpers/MiHelper.js'
     },
     DailyHelpers: {
-    require: './helpers/dailyHelper.js'
+      require: './helpers/dailyHelper.js'
     },
     NavigationHelper: {
       require: './helpers/NavigationHelper.js'
     },
     DailyHelper: {
-    require: './helpers/HelperEmilio.js'
+      require: './helpers/HelperEmilio.js'
     },
     MiHelperYoutube: {
-    require: './helpers/MiHelperYoutube.js'
+      require: './helpers/MiHelperYoutube.js'
     },
     RechargeHelper: {
       require: './helpers/RechargeHelper.js',
     },
   },
-  
+
   include: {
-    I: './steps_file.js', 
+    I: './steps_file.js',
     dailyPage: './pages/dailyPage.js',
     validacionesYoutubeSteps: './steps/videoYoutubeSteps.js',
     youtubeMainPage: './pages/youtubeMainPage.js',
-    youtubeVideoPage: './pages/youtubeVideoMainPage.js', 
+    youtubeVideoPage: './pages/youtubeVideoMainPage.js',
     youtubeLoginPage: './pages/youtubeLoginPage.js',
   },
   gherkin: {
-  features: './features/*.feature',
-  steps: [
-    './steps/pruebaSteps.js',
-    './steps/loginSteps.js',
-    './steps/youtubeLoginSteps.js',
-    './steps/validacionesYoutubeSteps.js',
-    './steps/dailySteps.js',
-    './steps/telcelSteps.js',
-    './steps/videoYoutubeSteps.js',
-    './steps/stepsEmilio.js'
-  ] 
+    features: './features/*.feature',
+    steps: [
+      './steps/pruebaSteps.js',
+      './steps/loginSteps.js',
+      './steps/youtubeLoginSteps.js',
+      './steps/validacionesYoutubeSteps.js',
+      './steps/dailySteps.js',
+      './steps/telcelSteps.js',
+      './steps/videoYoutubeSteps.js',
+      './steps/stepsEmilio.js',
+      './steps/armandoCargaDescargaMockeoSteps.js'
+    ]
   },
   plugins: {
     allure: {
