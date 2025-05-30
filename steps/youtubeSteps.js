09e711b03dd99070afb90e321dc9735b19e9a51c
Hector Larios <lariosh69@gmail.com>
2025-05-30 13:06:30 -0600

    damn it works too :|

--- steps/youtubeSteps.js
+++ steps/youtubeSteps.js
@@ -1,25 +1,16 @@
 const { I } = inject();
-const { url, res, IMG } = require('../env.vars');
+const { url, res, IMG } = require("../env.vars");
 Given("navego al la página de youtube", async () => {
-   I.amOnPage("https://www.youtube.com");
+  I.amOnPage("https://www.youtube.com");
 });
 
 When("busco el video {string}", async (query) => {
   I.waitForElement("[name='search_query']", 10);
-  I.usePuppeteerTo("mockear youtube navigation", async ({ page }) => {
-    await page.setRequestInterception(true);
-
-    page.on("request", (request) => {
-      if (request.url().includes("search?prettyPrint=")) {
-        console.log("Intercepting request to:", request.url());
-        request.respond({
-          status: 200,
-          contentType: "application/json",
-          body: JSON.stringify(res),
-        });
-      } else {
-        request.continue();
-      }
+  I.mockRoute(url, (req) => {
+    req.respond({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify(res),
     });
   });
 
@@ -27,12 +18,8 @@ When("busco el video {string}", async (query) => {
   I.pressKey("Enter");
 });
 
-
-Then(
-  "valido que secambiaron las miniaturas de los videos",
-  async () => {
-    const locator = `//img[@src="${IMG}"]`;
-    I.waitForElement(locator, 20);
-    I.say("Validando que las miniaturas de los videos se han cambiado");
-  }
-);
+Then("valido que secambiaron las miniaturas de los videos", async () => {
+  const locator = `//img[@src="${IMG}"]`;
+  I.waitForElement(locator, 20);
+  I.say("Validando que las miniaturas de los videos se han cambiado");
+});
