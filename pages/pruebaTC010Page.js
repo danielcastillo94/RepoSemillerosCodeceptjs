ab71158e13be2d8b6c2cabff78dc22a1b9292024
AnaRebo15 <anapaorebo15@gmail.com>
2025-10-09 13:48:04 -0600

    Ejercicio 4 - Feature TC010 - APRS

--- pages/pruebaTC010Page.js
+++ pages/pruebaTC010Page.js
@@ -3,6 +3,7 @@
 const { I } = inject();
 
 class footertelcelPage {
+    // Elementos de la página a los que se les aplicarán las pruebas
     elements = {
         footer: 'footer.telcel-footer',
         redesSociales: 'ul.contenido-redes-sociales li a',          // Son 4 de redes sociales
@@ -13,22 +14,26 @@ class footertelcelPage {
         I.amOnPage('https://www.telcel.com/');
     }
 
+    // Desplaza la página hasta el footer y espera a que termine de cargarse
     irAlFooter() {
         I.scrollTo(this.elements.footer);
         I.waitForElement(this.elements.footer, 10);
     }
 
+    // Verifica que cada enlace e icono de redes sociales sea visible y tenga la URL correcta
     verificarRedesSociales(linksEsperados) {
         linksEsperados.forEach(link => {
             I.waitForVisible(`${this.elements.redesSociales}[href="${link.url}"]`, 5);
-            I.seeElement(`${this.elements.redesSociales}[href="${link.url}"]`);
+            I.seeElement(`${this.elements.redesSociales}[href="${link.url}"]`);     // Comprueba el enlace
+            I.seeElement(`${this.elements.redesSociales}[href="${link.url}"] img`); // Comprueba la imagen
         });
     }
 
+    // Verifica que cada enlace de accesos rápidos sea visible y tenga la URL correcta y se vea el texto correcto
     verificarAccesosRapidos(linksEsperados) {
         linksEsperados.forEach(link => {
-            I.waitForVisible(`${this.elements.accesosRapidos}[href="${link.url}"]`, 5);
-            I.seeElement(`${this.elements.accesosRapidos}[href="${link.url}"]`);
+            I.waitForVisible(`${this.elements.accesosRapidos}[href="${link.url}"]`, 5); // Espera a que carguen los enlaces
+            I.see(link.texto, `${this.elements.accesosRapidos}[href="${link.url}"]`);   // Comprueba que el texto y el enlace sean correctos
         });
     }
 }
--- steps/pruebaTC010Steps.js
+++ steps/pruebaTC010Steps.js
@@ -3,38 +3,39 @@
 const { footertelcelPage } = inject();
 
 Given(/^el usuario está en la página principal$/, () => {
-  footertelcelPage.abrirPagina();
+    footertelcelPage.abrirPagina();
 });
 
 When(/^se desplaza al final del sitio$/, () => {
-  footertelcelPage.irAlFooter();
+    footertelcelPage.irAlFooter();
 });
 
 Then(/^los enlaces de redes sociales son visibles y apuntan a la URL correcta$/, () => {
-  const redesSocialesLinks = [
-    { nombreboton: 'Twitter', url: 'https://twitter.com/telcel' },
-    { nombreboton: 'Facebook', url: 'https://web.facebook.com/Telcel?_rdc=1&_rdr' },
-    { nombreboton: 'YouTube', url: 'https://www.youtube.com/user/Telceloficial' },
-    { nombreboton: 'Hola Telcel', url: 'https://holatelcel.com/' }
-  ];
-  footertelcelPage.verificarRedesSociales(redesSocialesLinks);
+    // Lista de links de las redes sociales y su valor en el atributo data-nombreboton
+    const redesSocialesLinks = [
+        { nombreboton: 'Twitter', url: 'https://twitter.com/telcel' },
+        { nombreboton: 'Facebook', url: 'https://web.facebook.com/Telcel?_rdc=1&_rdr' },
+        { nombreboton: 'YouTube', url: 'https://www.youtube.com/user/Telceloficial' },
+        { nombreboton: 'Hola Telcel', url: 'https://holatelcel.com/' }
+    ];
+    footertelcelPage.verificarRedesSociales(redesSocialesLinks);
 });
 
 Then(/^los accesos rápidos son visibles y apuntan a la URL correcta$/, () => {
-  const accesosRapidosLinks = [
-    { nombreboton: 'Tienda en línea', url: 'https://www.telcel.com/tienda/' },
-    { nombreboton: 'Planes Telcel Libre', url: '/personas/planes-de-renta/tarifas-y-opciones/telcel-libre' },
-    { nombreboton: 'Paquetes Amigo Sin Límite', url: '/personas/amigo/paquetes/paquetes-amigo-sin-limite' },
-    { nombreboton: 'Paquetes y Recargas', url: '/personas/compra-paquetes-y-recargas' },
-    { nombreboton: 'Viajero Internacional', url: '/personas/roaming/paquetes-y-precios#!plan-de-renta.html' },
-    { nombreboton: 'Experiencias', url: '/personas/experiencias-telcel' },
-    { nombreboton: 'Internet de las cosas', url: '/personas/internet-de-las-cosas/vida-conectada' },
-    { nombreboton: 'Cámbiate a Telcel', url: '/personas/portate/hazlo-ahora' },
-    { nombreboton: 'Promociones Plan de Renta', url: '/personas/planes-de-renta/promociones' },
-    { nombreboton: '¿Quién es Telcel?', url: '/personas/quien-es-telcel' },
-    { nombreboton: 'Políticas y códigos', url: '/personas/politicas-y-codigos' },
-    { nombreboton: 'Trabaja con Telcel', url: '/personas/trabaja-con-telcel/enviar-curriculum' }
-
-  ];
-  footertelcelPage.verificarAccesosRapidos(accesosRapidosLinks);
+    // Lista de links y texto de los accesos rápidos
+    const accesosRapidosLinks = [
+        { texto: 'Tienda en línea', url: 'https://www.telcel.com/tienda/' },
+        { texto: 'Planes Telcel Libre', url: '/personas/planes-de-renta/tarifas-y-opciones/telcel-libre' },
+        { texto: 'Paquetes Amigo Sin Límite', url: '/personas/amigo/paquetes/paquetes-amigo-sin-limite' },
+        { texto: 'Paquetes y Recargas', url: '/personas/compra-paquetes-y-recargas' },
+        { texto: 'Viajero Internacional', url: '/personas/roaming/paquetes-y-precios#!plan-de-renta.html' },
+        { texto: 'Experiencias', url: '/personas/experiencias-telcel' },
+        { texto: 'Internet de las cosas', url: '/personas/internet-de-las-cosas/vida-conectada' },
+        { texto: 'Cámbiate a Telcel', url: '/personas/portate/hazlo-ahora' },
+        { texto: 'Promociones Plan de Renta', url: '/personas/planes-de-renta/promociones' },
+        { texto: '¿Quién es Telcel?', url: '/personas/quien-es-telcel' },
+        { texto: 'Políticas y códigos', url: '/personas/politicas-y-codigos' },
+        { texto: 'Trabaja con Telcel', url: '/personas/trabaja-con-telcel/enviar-curriculum' }
+    ];
+    footertelcelPage.verificarAccesosRapidos(accesosRapidosLinks);
 });
