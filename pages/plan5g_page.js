1da002fff1ced3459b9ef34b97685751248ab0f2
MahiliFlores <mahiliflores@outlook.com>
2025-10-14 01:14:24 -0600

    Guardo cambios locales en plan5g

--- pages/plan5g_page.js
+++ pages/plan5g_page.js
@@ -1,37 +1,36 @@
 const { I } = inject();
 
 class plan5g_page {
-    fields = {  //Área de selectores  
-       Cookies: '[class="telcel-banner-aviso-cookies"]',
-      
+  fields = {  //Área de selectores  
+    Cookies: '[class="telcel-banner-aviso-cookies"]',
 
-    };
 
-    async visitPage() {
-        //Pagina de planes Telcel 
-        I.amOnPage('https://www.telcel.com/personas/planes-de-renta/tarifas-y-opciones/telcel-ultra');
-         I.wait(20);
-             if (await I.grabNumberOfVisibleElements(this.fields.Cookies)) {
-       I.click('//a[@id="acepto-cookies"]');
-      }
-       I.wait(10);
+  };
+
+  async visitPage() {
+    //Pagina de planes Telcel 
+    I.amOnPage('https://www.telcel.com/personas/planes-de-renta/tarifas-y-opciones/telcel-ultra');
+    I.wait(20);
+    if (await I.grabNumberOfVisibleElements(this.fields.Cookies)) {
+      I.click('//a[@id="acepto-cookies"]');
     }
 
-    seleccion5g() {
-        //Scroll a Telcel Ultra 5
-        I.scrollTo('//p[text()="Telcel Ultra 5"]');
-      I.wait(20);
-      // I.scrollTo('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[1]/p');
+  }
 
+  seleccion5g() {
+    //Scroll a Telcel Ultra 5
+    I.scrollTo('//p[text()="Telcel Ultra 5"]');
+    I.wait(15);
+    
+  }
 
-    }
+  verificacion5g() {  
+    I.waitForElement('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div/div[5]/div/div/a', 5);
+    I.click('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div/div[5]/div/div/a')
+   // I.seeElement('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div/div[5]/div/div/a');
+    // I.waitForElement('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div[2]/a',10);
 
-    verificacion5g() {
-        I.waitForElement('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div/div[5]/div/div/a', 5);
-        I.seeElement('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div/div[5]/div/div/a');
-        // I.waitForElement('//*[@id="pagina-contenido-a68fab363c"]/div[2]/div/div/div/div[2]/section/div/div/div/div[3]/div[2]/div[2]/a',10);
-
-    }
+  }
 
 }
 module.exports = new plan5g_page(); //exportar la clase
