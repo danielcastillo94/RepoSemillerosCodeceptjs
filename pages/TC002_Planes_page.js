62d33ec2e78f23e0a2d0efe615609e355e63f698
unknown <vheredia.ing@gmail.com>
2025-10-20 12:58:03 -0600

    Modificada funcion para localizar el mapa

--- pages/TC002_Planes_page.js
+++ pages/TC002_Planes_page.js
@@ -1,22 +1,26 @@
 const { I } = inject();
 
 class planes_5G {
-    page_elements={
+    page_elements = {
         banner5G: '//*[@id="accordion-9615"]/div/div[7]/div/div/div/a/div[2]',
-        seeMapButton:'//*[@id="accordion-9615"]/div/div[7]/div/div/div/a/div[2]/div/button',
+        seeMapButton: '//*[@id="accordion-9615"]/div/div[7]/div/div/div/a/div[2]/div/button',
+        mapiFrame: '#iframe-recarga3',
         mapGraph: '//html/body/div[2]/div[2]/div[1]/div/div/div[3]/div[1]/div[2]'
     }
 
-    goToPage(){
+    goToPage() {
         I.amOnPage('https://www.telcel.com/personas/la-red-de-mayor-cobertura/red-tecnologia/5g.html');
     }
 
-    goToMap(){
+    goToMap() {
         I.click('//*[@id="acepto-cookies"]');
         I.scrollTo(this.page_elements.banner5G);
         I.click(this.page_elements.seeMapButton);
-        I.waitForElement(this.page_elements.mapGraph, 10);
-        I.seeElement(this.page_elements.mapGraph);
+        I.waitForElement(this.page_elements.mapiFrame, 10);
+        within({ frame: locate(this.page_elements.mapiFrame).first() }, () => {
+            I.waitForElement(this.page_elements.mapGraph, 10);
+            I.seeElement(this.page_elements.mapGraph);
+        });
     }
 }
 
