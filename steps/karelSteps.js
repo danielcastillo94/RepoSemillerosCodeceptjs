ed783138a0effb4f5a6a4c5d23f514b59045a5e6
Hermes Alberto Delgado Diaz <hermes.ciencias@gmail.com>
2026-08-10 16:10:23 -0600

    Refactor + fix karelPage.js - Hermes

--- steps/karelSteps.js
+++ steps/karelSteps.js
@@ -121,6 +121,7 @@ When(/^se desplaza al final del sitio$/, () => {
 });
 
 Then(/^los enlaces de redes sociales son visibles y apuntan a la URL correcta$/, () => {
+
     // Lista de links de las redes sociales y su valor en el atributo data-nombreboton
     const redesSocialesLinks = [
         { nombreboton: 'Twitter', url: 'https://twitter.com/telcel' },
@@ -128,17 +129,17 @@ Then(/^los enlaces de redes sociales son visibles y apuntan a la URL correcta$/,
         { nombreboton: 'YouTube', url: 'https://www.youtube.com/user/Telceloficial' },
         { nombreboton: 'Hola Telcel', url: 'https://holatelcel.com/' }
     ];
+
     karelPage.verificarRedesSociales(redesSocialesLinks);
 });
 
 Then(/^los accesos rápidos son visibles y apuntan a la URL correcta$/, () => {
     // Lista de links y texto de los accesos rápidos
     const accesosRapidosLinks = [
-        { texto: 'Tienda en línea', url: 'https://www.telcel.com/tienda/' },
         { texto: 'Planes Telcel Libre', url: '/personas/planes-de-renta/tarifas-y-opciones/telcel-libre' },
         { texto: 'Paquetes Amigo Sin Límite', url: '/personas/amigo/paquetes/paquetes-amigo-sin-limite' },
         { texto: 'Paquetes y Recargas', url: '/personas/compra-paquetes-y-recargas' },
-        { texto: 'Viajero Internacional', url: '/personas/roaming/paquetes-y-precios#!plan-de-renta.html' },
+        { texto: 'Viajero Internacional', url: '/personas/roaming/paquetes-y-precios' },
         { texto: 'Experiencias', url: '/personas/experiencias-telcel' },
         { texto: 'Internet de las cosas', url: '/personas/internet-de-las-cosas/vida-conectada' },
         { texto: 'Cámbiate a Telcel', url: '/personas/portate/hazlo-ahora' },
@@ -149,6 +150,7 @@ Then(/^los accesos rápidos son visibles y apuntan a la URL correcta$/, () => {
     ];
     karelPage.verificarAccesosRapidos(accesosRapidosLinks);
 });
+
 //TC011-------------------------------------------------------------------------------------------------------------------
 
 Given(/^El usuario esta en simulacion movil$/, () =>  {
