0111f41845634088d618e02e6dafc076262786fd
Yoban-git <yoan-cavic21@hotmail.com>
2026-08-20 11:17:40 -0600

    .

--- codecept.conf.js
+++ codecept.conf.js
@@ -7,7 +7,7 @@ exports.config = {
     Playwright: {
       browser: "chromium",
       url: 'https://www.liverpool.com.mx/tienda/home',
-      show: true,
+      show: false,
       locale: "es-MX",
       video: true,
       pressKeyDelay: 100,
