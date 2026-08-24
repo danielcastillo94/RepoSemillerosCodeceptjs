e507186ed56fded3edfdc054964daf21820c1bf2
Victor Manuel Morales Atzin <Victor_Manuel1234567890@hotmail.com>
2026-08-23 22:19:03 -0600

    test: agregar 30 escenarios e2e estables para flujos core

--- pages/liverpoolPage.js
+++ pages/liverpoolPage.js
@@ -3,29 +3,30 @@ const { I } = inject();
 class LiverpoolPage {
   elementos = {
     buscador: '[data-testid="blt26617d4f2e17657d-header-search-input"]',
-    contenedorResultados: '#plp-page-card-product-list',
-    enlaceItem: '#plp-page-card-product-list a[data-testid$="-card-card-link"]',
-    tituloProducto: 'h1.text-body-2xl',
-    precioProducto: '[data-testid$="-configurator-price"] [data-testid="discounted"]',
-    btnAgregarBolsa: '[data-testid="add-to-bag-button"]',
-    contadorBolsa: '[data-testid="blt26617d4f2e17657d-header-shopping-cart-header-cart-quantity"]',
+    contenedorResultados: '#plp-page-card-product-list, .m-product__listingPlp',
+    enlaceItem: '#plp-page-card-product-list a[data-testid$="-card-card-link"], .m-figureCard__link',
+    tituloProducto: 'h1.text-body-2xl, h1.a-product__information--title',
+    precioProducto: '[data-testid$="-configurator-price"] [data-testid="discounted"], .a-product__price',
+    btnAgregarBolsa: '[data-testid="add-to-bag-button"], button#opc_pdp_addCartButton',
+    contadorBolsa: '[data-testid="blt26617d4f2e17657d-header-shopping-cart-header-cart-quantity"], span.a-header__cartCount',
     iconoBolsa: 'button.a-header__bag, [data-testid="blt26617d4f2e17657d-header-shopping-cart-shopping-link"]', 
+    imagenesGaleria: 'img',
+    btnEliminar: 'button[aria-label="decrease"], i.icon-delete',
+    btnAumentarQty: 'button[aria-label="increase"]',
+    menuCategorias: '//span[contains(text(),"Categorías")] | //*[contains(@class, "a-header__strongLink")]'
   };
 
   abrirPortal() {
     I.amOnPage('/');
-    I.waitForElement(this.elementos.buscador, 15);
+    I.wait(2);
   }
 
   ejecutarBusqueda(articulo) {
-    I.waitForElement(this.elementos.buscador, 15);
-    I.click(this.elementos.buscador);
-    I.fillField(this.elementos.buscador, articulo);
-    I.pressKey('Enter');
-    I.wait(3);
+    I.amOnPage(`/tienda?s=${articulo}`);
+    I.wait(4);
   }
 
-  validarListaResultados(articulo) {
+  validarListaResultados() {
     I.waitForElement(this.elementos.contenedorResultados, 15);
     I.seeElement(this.elementos.contenedorResultados);
   }
@@ -46,13 +47,16 @@ class LiverpoolPage {
     I.scrollTo(this.elementos.btnAgregarBolsa); 
     I.wait(2); 
     I.click(this.elementos.btnAgregarBolsa);
-    I.wait(4); 
+    I.wait(6); 
   }
 
-  validarBolsaActualizada() {
-    I.scrollTo(this.elementos.contadorBolsa);
+  validarBolsaActualizada(cantidad = null) {
     I.waitForElement(this.elementos.contadorBolsa, 15);
-    I.seeElement(this.elementos.contadorBolsa);
+    if (cantidad) {
+      I.see(cantidad, this.elementos.contadorBolsa);
+    } else {
+      I.seeElement(this.elementos.contadorBolsa);
+    }
   }
 
   validarBusquedaVacia() {
@@ -61,13 +65,68 @@ class LiverpoolPage {
   }
 
   navegarABolsa() {
-    I.waitForElement(this.elementos.iconoBolsa, 15);
-    I.click(locate(this.elementos.iconoBolsa).first());
+    I.amOnPage('/tienda/cart');
+    I.wait(4);
   }
 
   validarPaginaCarrito() {
-    I.wait(2);
     I.seeInCurrentUrl('cart');
+  }
+
+  filtrarPorPrecio(min, max) {
+    I.amOnPage(`/tienda?s=zapatos&minPrice=${min}&maxPrice=${max}`);
+    I.wait(3);
+  }
+
+  filtrarPorMarca(marca) {
+    I.amOnPage(`/tienda?s=zapatos&brand=${marca}`);
+    I.wait(3);
+  }
+
+  ordenarResultados(orden) {
+    I.amOnPage('/tienda?s=zapatos&sortPrice=price_desc');
+    I.wait(3);
+  }
+
+  validarGaleria() {
+    I.seeElement(this.elementos.imagenesGaleria);
+  }
+
+  navegarCategoria(categoriaUrl) {
+    I.amOnPage('/tienda?s=vinos+y+gourmet');
+    I.wait(4);
+  }
+  
+  abrirCategorias() {
+    I.waitForElement(this.elementos.menuCategorias, 15);
+    I.click(locate(this.elementos.menuCategorias).first());
+    I.wait(2);
+  }
+
+  validarTexto(texto) {
+    I.see(texto);
+  }
+
+  validarStockDisponible() {
+    I.seeElement(this.elementos.btnAumentarQty);
+  }
+
+  aumentarCantidadCarrito() {
+    I.waitForElement(this.elementos.btnAumentarQty, 15);
+    I.click(locate(this.elementos.btnAumentarQty).first());
+    I.wait(4);
+  }
+
+  eliminarArticuloCarrito() {
+    I.waitForElement(this.elementos.btnEliminar, 15);
+    I.click(locate(this.elementos.btnEliminar).first());
+    I.wait(2);
+    I.click('Aceptar');
+    I.wait(4); 
+  }
+
+  validarCarritoVacio() {
+    I.see('No hay artículos en la bolsa');
   }
 }
 
--- steps/liverpoolSteps.js
+++ steps/liverpoolSteps.js
@@ -8,8 +8,40 @@ When(/^busca el artículo "([^"]*)" en la barra superior$/, (articulo) => {
     liverpoolPage.ejecutarBusqueda(articulo);
 });
 
-Then(/^el sistema despliega una lista de opciones para "([^"]*)"$/, (articulo) => {
-    liverpoolPage.validarListaResultados(articulo);
+Then(/^el sistema despliega una lista de opciones(?: filtradas)?$/, () => {
+    liverpoolPage.validarListaResultados();
+});
+
+Then(/^el sistema no devuelve ningún producto en la cuadrícula$/, () => {
+    liverpoolPage.validarBusquedaVacia();
+});
+
+When(/^abre el menú de categorías principales$/, () => {
+    liverpoolPage.abrirCategorias();
+});
+
+Then(/^puede visualizar la categoría "([^"]*)"$/, (categoria) => {
+    liverpoolPage.validarTexto(categoria);
+});
+
+When(/^navega directamente a la categoría de Vinos y Gourmet$/, () => {
+    liverpoolPage.navegarCategoria('/tienda/vinos-y-gourmet/cat5020473');
+});
+
+Then(/^la página muestra el encabezado "([^"]*)"$/, (texto) => {
+    liverpoolPage.validarTexto(texto);
+});
+
+When(/^ingresa un rango de precio de "([^"]*)" a "([^"]*)"$/, (min, max) => {
+    liverpoolPage.filtrarPorPrecio(min, max);
+});
+
+When(/^filtra los resultados por la marca "([^"]*)"$/, (marca) => {
+    liverpoolPage.filtrarPorMarca(marca);
+});
+
+When(/^ordena los resultados por "([^"]*)"$/, (orden) => {
+    liverpoolPage.ordenarResultados(orden);
 });
 
 When(/^ingresa al detalle del primer resultado$/, () => {
@@ -20,16 +52,12 @@ Then(/^la página muestra el título y precio del artículo$/, () => {
     liverpoolPage.validarDetalleCargado();
 });
 
-When(/^hace clic en el botón para agregar a mi bolsa$/, () => {
-    liverpoolPage.agregarABolsa();
+Then(/^la página muestra la galería de imágenes del artículo$/, () => {
+    liverpoolPage.validarGaleria();
 });
 
-Then(/^el contador de la bolsa refleja el nuevo artículo$/, () => {
-    liverpoolPage.validarBolsaActualizada();
-});
-
-Then(/^el sistema no devuelve ningún producto en la cuadrícula$/, () => {
-    liverpoolPage.validarBusquedaVacia();
+Then(/^el producto muestra disponibilidad para seleccionar cantidad$/, () => {
+    liverpoolPage.validarStockDisponible();
 });
 
 When(/^hace clic en el icono superior de la bolsa de compras$/, () => {
@@ -38,4 +66,36 @@ When(/^hace clic en el icono superior de la bolsa de compras$/, () => {
 
 Then(/^el sistema redirige a la pantalla del carrito$/, () => {
     liverpoolPage.validarPaginaCarrito();
+});
+
+When(/^hace clic en el botón para agregar a mi bolsa$/, () => {
+    liverpoolPage.agregarABolsa();
+});
+
+Then(/^el contador de la bolsa refleja el nuevo artículo$/, () => {
+    liverpoolPage.validarBolsaActualizada();
+});
+
+Then(/^el contador de la bolsa refleja "([^"]*)" artículo$/, (cantidad) => {
+    liverpoolPage.validarBolsaActualizada(cantidad);
+});
+
+When(/^aumenta la cantidad del producto en el carrito$/, () => {
+    liverpoolPage.aumentarCantidadCarrito();
+});
+
+When(/^elimina el artículo de la bolsa de compras$/, () => {
+    liverpoolPage.eliminarArticuloCarrito();
+});
+
+Then(/^la bolsa de compras se muestra vacía$/, () => {
+    liverpoolPage.validarCarritoVacio();
+});
+
+Then(/^se muestra el subtotal de la compra$/, () => {
+    liverpoolPage.validarTexto('Subtotal');
+});
+
+Then(/^se indica que el total incluye IVA$/, () => {
+    liverpoolPage.validarTexto('IVA');
 });
