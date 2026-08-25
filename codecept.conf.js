c9914ba9a9ea67377690ed91c5385bd05c14e2f8
XimenaRoaz <ximenaroaz00@gmail.com>
2026-08-25 14:10:00 -0600

    fix: run playwright in headless mode

--- codecept.conf.js
+++ codecept.conf.js
@@ -7,7 +7,7 @@ exports.config = {
     Playwright: {
       browser: "chromium",
       url: 'https://www.liverpool.com.mx',
-      show: true,
+      show: false,
       locale: "es-MX"
     }
   },
